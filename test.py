dba41a77532f0278daafe7730c842313d6f7e0d4
Jordan Ye <yejordan8888@gmail.com>
2025-07-04 13:41:14 -0400

    Remove obsolete Python files related to various Codeforces problems
    This commit deletes several unused Python files, including A_Password_Length.py, cycles, cycles_counter.cpp, Cycles_in_Graph.py, Durability_Constrained_Transport.py, Edge_Contraction_Pieces.py, Expected_Maximum_Dice.py, Janmansh_and_Assignments.py, Server_Client_Strings.py, Stations_Distance.py, and various test files. The removal is part of a codebase cleanup effort to eliminate outdated or unnecessary files, ensuring a more maintainable and streamlined project structure.

--- test.py
+++
@@ -1,6 +0,0 @@
-mod=998244353
-inv6=pow(6,mod-2,mod)
-prob=[(k*inv6)%mod for k in range(7)]
-inv_prob=[0, pow(prob[1],mod-2,mod), pow(prob[2],mod-2,mod), pow(prob[3],mod-2,mod), pow(prob[4],mod-2,mod),pow(prob[5],mod-2,mod),pow(prob[6],mod-2,mod)]
-print(prob)
-print(inv_prob)
